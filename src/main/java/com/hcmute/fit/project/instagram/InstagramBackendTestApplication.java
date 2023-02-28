1968b5445bb23e4649cd072193146e5d9606caf4
ngthvan1612 <ngthvan1612@gmail.com>
2023-02-28 15:51:05 +0700

    Initial

---
+++ src/main/java/com/hcmute/fit/project/instagram/InstagramBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.fit.project.instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramBackendTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramBackendTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcmute/fit/project/instagram/InstagramBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmute.fit.project.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramBackendTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
